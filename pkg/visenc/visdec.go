2dc3fe65fa3cde35bcd7203c83381c8d03aa7e3a
Adam Olech <nddr89@gmail.com>
2022-01-11 01:04:07 +0100

    pkg/visenc: load image

--- pkg/visenc/visdec.go
+++ pkg/visenc/visdec.go
@@ -1,9 +1,12 @@
 package visenc
 
 import (
+	"errors"
 	"fmt"
 	"image"
+	"os"
 	"path/filepath"
+	"strconv"
 	"strings"
 )
 
@@ -13,10 +16,39 @@ type VisDec struct {
 
 // path is the path to any of the shares
 func (v *VisDec) LoadFromFile(path string) error {
+	shareFileExt := filepath.Ext(path)
 	splitShareName := strings.Split(
-		strings.TrimSuffix(path, filepath.Ext(path)),
+		strings.TrimSuffix(path, shareFileExt),
 		".",
 	)
+
+	if _, err := strconv.Atoi(splitShareName[len(splitShareName)-1]); err != nil {
+		return errors.New("input file does not end in <int>.<ext>")
+	}
+
+	v.Shares = make([]image.Image, 2)
+
+	for i := 1; i < 3; i++ {
+		imageFile, err := os.Open(
+			fmt.Sprintf("%s.%d%s",
+				splitShareName[0], i, shareFileExt,
+			),
+		)
+
+		defer imageFile.Close()
+
+		if err != nil {
+			return err
+		}
+
+		shareImage, _, err := image.Decode(imageFile)
+
+		if err != nil {
+			return err
+		}
+
+		v.Shares[i-1] = shareImage
+	}
 
 	fmt.Println(splitShareName)
 
